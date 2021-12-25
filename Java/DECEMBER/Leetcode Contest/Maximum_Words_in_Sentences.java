2043d9d1ee619a3564c5f2758cea3e088a3a23d5
Archan Rajaram R <archanssn2015@gmail.com>
2021-12-25 20:50:22 +0530

    25/12/2021 -> Maximum Number of Words Found in Sentences - COMMENTS UPDATED [Chrismas ]

--- Java/DECEMBER/Leetcode Contest/Maximum_Words_in_Sentences.java
+++ Java/DECEMBER/Leetcode Contest/Maximum_Words_in_Sentences.java
@@ -2,15 +2,10 @@
  Maximum Number of Words Found in Sentences
  
 A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
-
 You are given an array of strings sentences, where each sentences[i] represents a single sentence.
-
 Return the maximum number of words that appear in a single sentence.
 
- 
-
 Example 1:
-
 Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
 Output: 6
 Explanation: 
