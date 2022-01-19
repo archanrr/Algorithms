e5491749bee2b419c14a97ff0fb2a985146597d3
Archan Rajaram R <archanssn2015@gmail.com>
2022-01-19 20:33:19 +0530

    19/1/2022:-> [HashSet][Medium] Cycle Start [National Popcorn Day 2022]
    #Leetcode #January2022

---
+++ Java/January-2022/LinkedListCycleStart.java
@@ -0,0 +1,32 @@
+
+/*
+19th January 2022
+
+142. Linked List Cycle II
+
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+*/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycleStart {
+   public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> cycle = new HashSet<ListNode>();
+        ListNode temp = head;
+        while(!cycle.contains(temp) && temp!=null){
+            cycle.add(temp);
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
