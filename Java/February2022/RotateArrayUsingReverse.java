9ad67bfb47e9b0a12a4ebe6e8a2f344ae9316b9c
Archan Rajaram R <archanssn2015@gmail.com>
2022-02-12 22:46:08 +0530

    12/2/2022: 189. Rotate Array using reverse
    #leetcode #Algorithm #February-2022

---
+++ Java/February2022/RotateArrayUsingReverse.java
@@ -0,0 +1,34 @@
+/*
+12/2/2022: 189. Rotate Array
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+*/
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(k==0||k==nums.length||nums.length==1){
+            return;
+        } else if(k>nums.length){
+            k%=nums.length;
+        }
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+    public static void reverse(int[] arr, int i, int j) {
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
