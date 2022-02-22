96dda2ecf9213ca14616c7fb2753ec93f3e585b9
Archan Rajaram R <archanssn2015@gmail.com>
2022-02-22 17:41:28 +0530

    22/2/2022 : 733. Flood Fill [Today is Both palindrome and Ambigram]
    #Leetcode #February-2022

---
+++ Java/February2022/FloodFill.java
@@ -0,0 +1,116 @@
+/*
+22/2/2022 : 733. Flood Fill [Both pallindrome and Ambigram]
+{
+The last palindromic date that we saw, however, was February 2, 2022 (earlier this month). Both pallindrome and Ambigram
+In 2020, February 2 was also a palindromic date. 
+But, let us point out that such dates are rare every millennium. 
+This millennium we have only 60 palindromic dates, and we have already wasted some of them. 
+Therefore, if you are someone who believes in luck, and fate, block your calendar today, and don’t let today go by insignificantly.
+
+Next -> Feb 8, 2080
+
+Achievement -> Bluethooth FM Transmitter for Maruthi Suzuki 2014 vxi
+
+@Trimble
+@Samsung
+
+532,726.10								547351.48			-14,625.38	
+												
+Symbol	"Exchange
+(Optional)"	Stock Name	Category	Current Price	Average Price	Current Profit/Loss	Net Profit/loss	Price Change	% Change	# Shares	Current Value	Invested Value
+INFY	NSE	Infosys Ltd	A-IT	1,733.00	1,769.64	-36.64	-1832.00	+2.30	+0.13%	50	86,650.00	88482.00
+HCLTECH	NSE	HCL Technologies Ltd	A-IT	1,152.90	1,163.71	-10.81	-756.70	-11.85	-1.02%	70	80,703.00	81459.70
+WIPRO	NSE	Wipro Limited	A-IT	562.20	618.46	-56.26	-7032.50	-8.60	-1.51%	125	70,275.00	77307.50
+TECHM	NSE	Tech Mahindra Ltd	A-IT	1,415.00	1,493.15	-78.15	-3516.75	-3.20	-0.23%	45	63,675.00	67191.75
+MINDAIND	NSE	Minda Industries Ltd	Auto Ancillaries	971.00	984.57	-13.57	-474.95	+35.60	+3.81%	35	33,985.00	34459.95
+SBIN	NSE	State Bank of India	BANK	497.90	480.40	+17.50	+525.00	-13.95	-2.73%	30	14,937.00	14412.00
+TATAMOTORS	NSE	Tata Motors Limited Fully Paid Ord. Shrs	Automobile	478.50	474.43	+4.07	+122.10	-15.95	-3.23%	30	14,355.00	14232.90
+AXISBANK	NSE	Axis Bank Ltd	BANK	779.05	673.32	+105.73	+2114.60	-9.80	-1.24%	20	15,581.00	13466.40
+M&M	NSE	Mahindra & Mahindra Limited	Automobile	855.60	833.35	+22.25	+333.75	+14.50	+1.72%	15	12,834.00	12500.25
+ICICIBANK		ICICI Bank Ltd	BANK	750.25	750.37	-0.12	-1.80	-4.20	-0.56%	15	11,253.75	11255.55
+TATASTEEL	NSE	Tata Steel Limited Fully Paid Ord. Shrs	STEEL	1,134.00	1,116.41	+17.59	+175.90	-47.85	-4.05%	10	11,340.00	11164.10
+CANBK	NSE	Canara Bank Ltd	BANK	230.55	213.98	+16.57	+828.50	-2.40	-1.03%	50	11,527.50	10699.00
+ZENSARTECH		Zensar Technologies Limited	A-IT	337.00	406.57	-69.57	-1739.25	-11.80	-3.38%	25	8,425.00	10164.25
+IEX	NSE	Indian Energy Exchange Ltd	misscel	204.90	259.23	-54.33	-1792.89	-5.30	-2.52%	33	6,761.70	8554.59
+IRCTC	NSE	Indian Railway Ctrng nd Trsm Corp Ltd	Railways	789.05	840.96	-51.91	-519.10	-14.35	-1.79%	10	7,890.50	8409.60
+TATAPOWER	NSE	Tata Power Company Limited	POWER	223.20	221.41	+1.79	+62.65	-1.80	-0.80%	35	7,812.00	7749.35
+TATACHEM		Tata Chemicals Limited	CHEMICALS	869.50	867.87	+1.63	+11.41	-7.40	-0.84%	7	6,086.50	6075.09
+INDUSINDBK		Indusind Bank Ltd	BANK	939.00	845.60	+93.40	+560.40	-13.45	-1.41%	6	5,634.00	5073.60
+MINDTECK		Mindteck (India) Ltd	A-IT	125.30	165.06	-39.76	-1192.80	-6.55	-4.97%	30	3,759.00	4951.80
+ASHOKLEY	NSE	Ashok Leyland Ltd	Automobile	124.65	121.70	+2.95	+118.00	-0.25	-0.20%	40	4,986.00	4868.00
+FEDERALBNK	NSE	Federal Bank Ltd	BANK	99.10	87.54	+11.56	+578.00	-0.65	-0.65%	50	4,955.00	4377.00
+IGL	NSE	Indraprastha Gas Limited	GAS	360.60	400.16	-39.56	-395.60	-9.95	-2.69%	10	3,606.00	4001.60
+MIDHANI		Mishra Dhatu Nigam Ltd	STEEL	172.45	183.00	-10.55	-211.00	+1.35	+0.79%	20	3,449.00	3660.00
+GREAVESCOT	NSE	Greaves Cotton Ltd	Capital Goods-Non Electrical Equipment	171.50	143.35	+28.15	+703.75	-4.15	-2.36%	25	4,287.50	3583.75
+GLENMARK	NSE	Glenmark Pharmaceuticals Ltd	Pharma	442.80	499.75	-56.95	-398.65	-12.80	-2.81%	7	3,099.60	3498.25
+EXIDEIND	NSE	Exide Industries Ltd	Auto Ancillaries	151.40	167.39	-15.99	-319.80	-2.85	-1.85%	20	3,028.00	3347.80
+BHEL	NSE	Bharat Heavy Electricals Limited	Electric Equip	49.20	57.17	-7.97	-398.50	-1.30	-2.57%	50	2,460.00	2858.50
+HINDCOPPER	NSE	Hindustan Copper Ltd.	Non Ferrous Metals	114.80	123.47	-8.67	-173.40	-2.45	-2.09%	20	2,296.00	2469.40
+PNB	NSE	Punjab National Bank	BANK	37.05	38.29	-1.24	-62.00	-0.45	-1.20%	50	1,852.50	1914.50
+HINDALCO	NSE	Hindalco Industries Ltd	Non Ferrous Metals	516.50	438.76	+77.74	+310.96	+4.50	+0.88%	4	2,066.00	1755.04
+CHEMBOND	NSE	Chembond Chemicals Ltd	CHEMICALS	183.00	209.48	-26.48	-211.84	-0.40	-0.22%	8	1,464.00	1675.84
+RBLBANK	NSE	RBL Bank Ltd	BANK	138.45	163.53	-25.08	-250.80	+0.50	+0.36%	10	1,384.50	1635.30
+MOREPENLAB	NSE	Morepen Laboratories Limited	Pharma	40.25	52.32	-12.07	-362.10	-0.50	-1.23%	30	1,207.50	1569.60
+ITC	NSE	ITC Ltd	Tobacco	214.65	213.41	+1.24	+8.68	-3.35	-1.54%	7	1,502.55	1493.87
+Redington		Redington (India) Ltd	TRADING	153.75	145.35	+8.40	+84.00	-2.40	-1.54%	10	1,537.50	1453.50
+ENGINERSIN		Engineers India Limited	INFRA	61.75	70.17	-8.42	-168.40	-1.25	-1.98%	20	1,235.00	1403.40
+GAIL		GAIL (India) Limited	GAS	134.55	131.01	+3.54	+35.40	-1.75	-1.28%	10	1,345.50	1310.10
+KELLTONTEC		Kellton Tech Solutions Ltd	A-IT	81.70	54.83	+26.87	+537.40	-4.25	-4.94%	20	1,634.00	1096.60
+UJJIVANSFB	NSE	Ujjivan Small Finance Bank Ltd	BANK	16.95	18.71	-1.76	-88.00	-0.55	-3.14%	50	847.50	935.50
+IRFC	NSE	Indian Railway Finance Corp Ltd	FINANCE	21.80	23.24	-1.44	-57.60	-0.25	-1.13%	40	872.00	929.60
+GMRINFRA	nse	GMR Infrastructure Ltd	INFRA	38.15	44.21	-6.06	-121.20	-0.95	-2.43%	20	763.00	884.20
+YESBANK	NSE	Yes Bank Limited	BANK	13.65	12.78	+0.87	+56.55	-	-	65	887.25	830.70
+INFIBEAM	nse	Infibeam Avenues Ltd	A-IT	38.75	41.31	-2.56	-51.20	-1.20	-3.00%	20	775.00	826.20
+KBCGLOBAL	NSE	KBC Global Ltd	Realty	15.50	16.35	-0.85	-42.50	-0.20	-1.27%	50	775.00	817.50
+IOB	NSE	Indian Overseas Bank	BANK	18.25	20.40	-2.15	-86.00	-0.55	-2.93%	40	730.00	816.00
+BANKBARODA		Bank of Baroda Ltd	BANK	104.50	79.78	+24.72	+247.20	+0.75	+0.72%	10	1,045.00	797.80
+COUNCODOS		Country Condo's Ltd	Realty	5.50	3.76	+1.74	+348.00	-0.05	-0.90%	200	1,100.00	752.00
+AIRAN	NSE	Airan Ltd	A-IT	20.55	24.13	-3.58	-107.40	-1.05	-4.86%	30	616.50	723.90
+PTL	NSE	PTL Enterprises Ltd	Misscel	29.60	35.87	-6.27	-125.40	-0.55	-1.82%	20	592.00	717.40
+HCC	NSE	Hindustan Construction Company Ltd	Construction 	16.20	16.15	+0.05	+2.00	+0.15	+0.93%	40	648.00	646.00
+INDOSOLAR		Indosolar Limited	Electric Equipment	4.05	5.31	-1.26	-126.00	-0.20	-4.71%	100	405.00	531.00
+DOLATALGO	nse	Dolat Algotech Ltd	Invst Banking	77.35	84.45	-7.10	-35.50	-3.50	-4.33%	5	386.75	422.25
+SUZLON	NSE	Suzlon Energy Ltd	Electric Equipment	9.20	7.22	+1.98	+99.00	-0.45	-4.66%	50	460.00	361.00
+NILASPACES		Nila Spaces Ltd	Construction 	3.80	3.21	+0.59	+59.00	-0.20	-5.00%	100	380.00	321.00
+JPPOWER	NSE	Jaiprakash Power Ventures Limited	Power	6.75	5.86	+0.89	+44.50	-0.55	-7.53%	50	337.50	293.00
+COMPUSOFT		Compucom Software Ltd	A-IT	22.45	16.50	+5.95	+59.50	-1.15	-4.87%	10	224.50	165.00
+
+
+Read more at:
+https://economictimes.indiatimes.com/magazines/panache/feb-22-2022-is-palindrome-day-block-your-calendar-for-something-important-auspicious-today/articleshow/89743780.cms?utm_source=contentofinterest&utm_medium=text&utm_campaign=cppst
+}
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with newColor.
+
+Return the modified image after performing the flood fill.
+
+Example 1:
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+*/
+class Solution {
+    public int[][] floodfillHelper(int[][] image, int sr, int sc, int newColor,int color) {
+        if(sr<0 || sr>=image.length || sc<0 || sc>=image[sr].length) return image;
+        if(image[sr][sc] == newColor) return image;
+        if(image[sr][sc] != color) return image;
+        image[sr][sc] = newColor;
+        floodfillHelper(image,sr-1,sc,newColor,color);
+        floodfillHelper(image,sr+1,sc,newColor,color);
+        floodfillHelper(image,sr,sc-1,newColor,color);
+        floodfillHelper(image,sr,sc+1,newColor,color);
+        return image;
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if(color == newColor){
+            return image;
+        }
+        return floodfillHelper(image,sr,sc,newColor,color);
+    } 
+}
