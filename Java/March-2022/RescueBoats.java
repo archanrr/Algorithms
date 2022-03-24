cdc95bb66c1bbb901f16fc45310e749364b04d7d
Archan Rajaram R <archan.rajaram1@gmail.com>
2022-03-24 09:44:40 -0700

    24/3/2022 : 881. Boats to Save People [ordered lenovo smart watch s2 pro]
    #Leetcode #March-2022

---
+++ Java/March-2022/RescueBoats.java
@@ -0,0 +1,29 @@
+/*
+24/3/2022 : 881. Boats to Save People [ordered lenovo smart watch s2 pro]
+You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person.
+*/
+class RescueBoats {
+    public int numRescueBoats(int[] people, int limit) {
+        int boatCount = 0;
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right){
+            int sum = people[left] + people[right];
+            if(sum <= limit){
+                boatCount++;
+                left++;
+                right--;
+            }
+            else{
+                boatCount++;
+                right--;
+            }
+        }
+        return boatCount;
+    }
+}
